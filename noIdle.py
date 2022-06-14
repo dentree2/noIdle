9af96fa27874f984e9cf6a4d5fdaa1e756ee2d95
dentree2 <97978661+dentree2@users.noreply.github.com>
2022-06-14 13:55:20 -0500

    Create noIdle.py

---
+++ noIdle.py
@@ -0,0 +1,17 @@
+import pyautogui
+import time
+from datetime import datetime
+
+print("Script is running... Press CTRL+C in the console to quit.")
+while(True): 
+    firstMousePos = pyautogui.position()
+    time.sleep(180)
+    secondMousePos = pyautogui.position()
+    if firstMousePos == secondMousePos: 
+        print("No motion:", secondMousePos, " at {}".format(datetime.now().time()))
+        pyautogui.moveTo(40,40)
+        for i in range(40,80):
+            pyautogui.moveTo(i*5,i*10)
+        pyautogui.press("shift")
+    else:
+        print("Motion:   ", secondMousePos, " at {}".format(datetime.now().time()))
