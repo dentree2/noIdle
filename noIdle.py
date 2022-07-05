b12bfd0b77865e49a7487f60e0cb117dd2caf5a5
dentree2 <97978661+dentree2@users.noreply.github.com>
2022-07-05 14:11:48 -0500

    Formatting update
    No code changes, just adding whitespace, removing unnecessary lines, parentheses.

--- noIdle.py
+++ noIdle.py
@@ -1,17 +1,17 @@
 import pyautogui
 import time
 from datetime import datetime
-
 print("Script is running... Press CTRL+C in the console to quit.")
-while(True): 
+while True:
     firstMousePos = pyautogui.position()
     time.sleep(180)
     secondMousePos = pyautogui.position()
-    if firstMousePos == secondMousePos: 
+    if firstMousePos == secondMousePos:
         print("No motion:", secondMousePos, " at {}".format(datetime.now().time()))
-        pyautogui.moveTo(40,40)
-        for i in range(40,80):
-            pyautogui.moveTo(i*5,i*10)
+        pyautogui.moveTo(40, 40)
+        for i in range(40, 80):
+            pyautogui.moveTo(i * 5, i * 10)
+        # pyautogui.click()
         pyautogui.press("shift")
     else:
         print("Motion:   ", secondMousePos, " at {}".format(datetime.now().time()))
